ab7c0841ea83817a8e14353f37f15b7bea1c8142
mipierc <mpierce2602@gmail.com>
2022-01-20 14:44:59 -0500

    implements working generator

---
+++ main.js
@@ -0,0 +1,5 @@
+function generate() {
+    let ranNum = Math.floor(Math.random() * quoteArr.length)
+    document.querySelector("blockquote").innerText = quoteArr[ranNum].line;
+    document.querySelector("cite").innerText = quoteArr[ranNum].movie;
+}
---
+++ quotes.js
@@ -0,0 +1,26 @@
+const quoteArr = [
+    {
+        "line": "Use the force, Luke",
+        "movie": "Star Wars"
+    },
+    {
+        "line": "Fill",
+        "movie": "Movie1"        
+    },
+    {
+        "line": "Empty",
+        "movie": "Movie2"        
+    },
+    {
+        "line": "Word",
+        "movie": "Movie3"        
+    },
+    {
+        "line": "Stop",
+        "movie": "Movie4"        
+    },
+    {
+        "line": "Go",
+        "movie": "Movie5"        
+    }
+]
