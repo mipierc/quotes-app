cd6d7f531acef286a8ecb6262e144d450733a501
Michael Pierce <mpierce2602@gmail.com>
2022-01-22 16:23:15 -0500

    styles index page, includes office quotes

--- main.js
+++ main.js
@@ -1,6 +1,80 @@
 function generate() {
-    let ranNum = Math.floor(Math.random() * quoteArr.length)
+    let ranNum = Math.floor(Math.random() * quoteArr.length);
     document.querySelector("blockquote").innerText = quoteArr[ranNum].line;
-    document.querySelector("cite").innerText = quoteArr[ranNum].movie;
+    document.querySelector("cite").innerText = quoteArr[ranNum].author;
 }
 generate()
+
+function generate_background() {
+    let ranNum = Math.floor(Math.random() * 7);
+    var element = document.getElementById("background");
+    switch (ranNum) {
+        case 0:
+            element.classList.add("bg-pink");
+            element.classList.remove("bg-teal");
+            element.classList.remove("bg-red");
+            element.classList.remove("bg-yellow");
+            element.classList.remove("bg-purple");
+            element.classList.remove("bg-teal");
+            element.classList.remove("bg-blue");
+            element.classList.remove("bg-green");
+            break;
+        case 1:
+            element.classList.add("bg-teal");
+            element.classList.remove("bg-pink");
+            element.classList.remove("bg-red");
+            element.classList.remove("bg-yellow");
+            element.classList.remove("bg-purple");
+            element.classList.remove("bg-blue");
+            element.classList.remove("bg-green");
+            break;
+        case 2:
+            element.classList.add("bg-red");
+            element.classList.remove("bg-teal");
+            element.classList.remove("bg-pink");
+            element.classList.remove("bg-yellow");
+            element.classList.remove("bg-purple");
+            element.classList.remove("bg-blue");
+            element.classList.remove("bg-green");
+            break;
+        case 3:   
+            element.classList.add("bg-yellow"); 
+            element.classList.remove("bg-teal");
+            element.classList.remove("bg-red");
+            element.classList.remove("bg-pink");
+            element.classList.remove("bg-purple");
+            element.classList.remove("bg-green");
+            element.classList.remove("bg-blue");
+            break;
+        case 4:
+            element.classList.add("bg-purple");
+            element.classList.remove("bg-teal");
+            element.classList.remove("bg-red");
+            element.classList.remove("bg-yellow");
+            element.classList.remove("bg-pink");
+            element.classList.remove("bg-green");
+            element.classList.remove("bg-blue");
+            break;
+        case 5:
+            element.classList.add("bg-green");
+            element.classList.remove("bg-teal");
+            element.classList.remove("bg-red");
+            element.classList.remove("bg-yellow");
+            element.classList.remove("bg-purple");
+            element.classList.remove("bg-pink");
+            element.classList.remove("bg-blue");
+            break;
+        case 6:
+            element.classList.add("bg-blue");
+            element.classList.remove("bg-teal");
+            element.classList.remove("bg-red");
+            element.classList.remove("bg-yellow");
+            element.classList.remove("bg-purple");
+            element.classList.remove("bg-green");
+            element.classList.remove("bg-pink");  
+            break;       
+        default:
+            break;
+    }
+}
+generate_background()
--- quotes.js
+++ quotes.js
@@ -1,26 +1,38 @@
 const quoteArr = [
     {
-        "line": "Use the force, Luke",
-        "movie": "Star Wars"
+        "line": "Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.",
+        "author": "Michael Scott"
     },
     {
-        "line": "Fill",
-        "movie": "Movie1"        
+        "line": "I talk a lot, so I’ve learned to tune myself out.",
+        "author": "Kelly Kapoor"        
     },
     {
-        "line": "Empty",
-        "movie": "Movie2"        
+        "line": "If I don’t have some cake soon, I might die.",
+        "author": "Stanley Hudson"        
     },
     {
-        "line": "Word",
-        "movie": "Movie3"        
+        "line": "No, Rose, they are not breathing. And they have no arms or legs … Where are they? You know what? If we come across somebody with no arms or legs, do we bother resuscitating them? I mean, what quality of life do we have there?",
+        "author": "Michael Scott"        
     },
     {
-        "line": "Stop",
-        "movie": "Movie4"        
+        "line": "Today, smoking is going to save lives.",
+        "author": "Dwight Schrute"        
     },
     {
-        "line": "Go",
-        "movie": "Movie5"        
+        "line": "I just want to lie on the beach and eat hot dogs. That’s all I’ve ever wanted.",
+        "author": "Kevin Malone"        
+    },
+    {
+        "line": "If I were buying my coffin, I would get one with thicker walls so you couldn’t hear the other dead people.",
+        "author": "Dwight Schrute"        
+    },
+    {
+        "line": "I’m not superstitious, but I am a little stitious.",
+        "author": "Michael Scott"        
+    },
+    {
+        "line": "There’s a lot of beauty in ordinary things. Isn’t that kind of the point?",
+        "author": "Pam Beesly"        
     }
 ]
